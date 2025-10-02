e6b7f7bc0cface27a70c9571882f06e7f87482bf
Anshh <anshchachra0@gmail.com>
2025-10-02 23:24:22 +0530

    added db route

--- kanji-backend/src/config/db.ts
+++ kanji-backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/KanjiDatabase");
+    await mongoose.connect("mongodb+srv://Project:ansh12345@localconnect.px4km.mongodb.net/KanjiDatabase");
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed", error);
